02010f92638a0e7ae2e84251c281d7b0ecedd6ab
Antti Laiti <antti@laiti.fi>
2021-07-28 09:38:12 +0300

    Login and logout ready

--- cypress/integration/modem.js
+++ cypress/integration/modem.js
@@ -1,12 +1,19 @@
+const url = Cypress.env('ESA_URL');
+
 describe('Login to 4G router and change band', () => {
     it('Login to 4G router', () => {
-        cy.viewport(1280,1280);
-        cy.visit(Cypress.env('ESA_URL'));
+        cy.viewport(1024,768);
+        cy.visit(url);
         cy.contains('LTE3301').should('exist');
         cy.contains('Welcome to the configuration interface. Please enter username and password to login.').should('exist');
         cy.get('input[name=un]').type(Cypress.env('ESA_USERNAME'));
         cy.get('input[name=pw]').type(Cypress.env('ESA_PASSWORD'));
         cy.get('input[type=submit]').click();
+    })
+    it('Logout from 4G router', () => {
+        cy.viewport(1024,768);
+        cy.visit(url);
+        cy.window().then(win => win.actionLogout());
     })
 })
   
